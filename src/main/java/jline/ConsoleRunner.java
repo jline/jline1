1256af3027811d4369c8b3d454731f19d1aada1e
Charles Oliver Nutter <headius@headius.com>
2010-05-13 16:03:17 +0200

    Fix for 1806950: No way to replace ConsoleRunner TAB-completors

--- src/main/java/jline/ConsoleRunner.java
+++ src/main/java/jline/ConsoleRunner.java
@@ -17,6 +17,10 @@ import java.util.*;
  *  @author  <a href="mailto:mwp1@cornell.edu">Marc Prud'hommeaux</a>
  */
 public class ConsoleRunner {
+    private static ConsoleReader reader;
+
+    public static ConsoleReader getReader() { return reader; }
+
     public static final String property = "jline.history";
 
     public static void main(final String[] args) throws Exception {
@@ -36,7 +40,7 @@ public class ConsoleRunner {
         String mainClass = (String) argList.remove(0);
 
         // setup the inpout stream
-        ConsoleReader reader = new ConsoleReader();
+        reader = new ConsoleReader();
 
         if (historyFileName != null) {
             reader.setHistory(new History (new File
