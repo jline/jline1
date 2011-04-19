2d6cb19b4d31457c8092d6491f40ac17d4995127
Ståle W. Pedersen <stalep@gmail.com>
2011-04-20 00:17:30 +0200

    added several tests

--- src/test/java/jline/ViModeTest.java
+++ src/test/java/jline/ViModeTest.java
@@ -1,5 +1,7 @@
 package jline;
 
+import java.io.IOException;
+
 /**
  * @author Ståle W. Pedersen <stale.pedersen@jboss.org>
  */
@@ -52,9 +54,9 @@ public class ViModeTest extends JLineTestCase {
         Buffer b = new Buffer("foo   bar...  Foo-Bar.");
         b.append(27) // esc
          .append(66) // shift-b
-         .append(100).append(119) // dw
+         .append(100).append(98) // db
          .append(10);
-        assertBufferViMode("foo   bar...  -Bar.", b, true);
+        assertBufferViMode("foo   barFoo-Bar.", b, true);
 
         b = new Buffer("foo   bar...  Foo-Bar.");
         b.append(27) // esc
@@ -73,6 +75,53 @@ public class ViModeTest extends JLineTestCase {
          .append(100).append(87) // dW
          .append(10); // enter
         assertBufferViMode("foo   barFoo-Bar.", b, true);
+
+        b = new Buffer("foo   bar...   Foo-Bar.");
+        b.append(27) // esc
+         .append(66) // B
+         .append(100).append(66) // dB
+         .append(10);
+        assertBufferViMode("foo   Foo-Bar.", b, true);
+
+        b = new Buffer("foo   bar...   Foo-Bar.");
+        b.append(27) // esc
+         .append(48) // 0
+         .append(119).append(119) // ww
+         .append(105) // i
+         .append("-bar")
+         .append(27)
+         .append(66)
+         .append(100).append(119) //dw
+         .append(120) // x
+         .append(100).append(66) //dB
+         .append(10);
+        assertBufferViMode("bar...   Foo-Bar.", b, true);
+    }
+
+    public void testRepeatAndEdit() throws IOException {
+        Buffer b = new Buffer("/cd /home/foo; ls; cd Desktop; ls ../");
+        b.append(27) // esc
+         .append(48) // 0
+         .append(119).append(119).append(119).append(119).append(119) // 5x w
+         .append(99).append(119) // cw
+         .append("bar")
+         .append(27) // esc
+         .append(87) // W
+         .append(100).append(119) //dw
+         .append(46) // .
+         .append(10);
+        assertBufferViMode("/cd /home/bar; cd Desktop; ls ../", b, true);
+
+        b = new Buffer("/cd /home/foo; ls; cd Desktop; ls ../");
+        b.append(27) // esc
+         .append(66) // B
+         .append(68) // D
+         .append(66) // B
+         .append(46) // .
+         .append(66) // B
+         .append(46) // .
+         .append(10);
+        assertBufferViMode("/cd /home/foo; ls; cd ", b, true);
     }
 
 }
